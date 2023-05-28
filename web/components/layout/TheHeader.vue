8d22bfe4ccbd8c2ce423801968beef12019adcc8
zhong1016 <tanyzhong@gmail.com>
2023-05-28 15:19:19 +0800

    feat: update link class

--- web/components/layout/TheHeader.vue
+++ web/components/layout/TheHeader.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts"></script>
 <template>
   <header class="h-16 w-full flex flex-wrap">
-    <div class="w-1/2 flex justify-center lg:block">
-      <nuxt-link to="/" class="text-2xl font-bold h-full flex items-center">
-        SinShui
-      </nuxt-link>
+    <div class="w-1/2 flex justify-center items-center lg:justify-start">
+      <nuxt-link to="/" class="text-2xl font-bold"> SinShui </nuxt-link>
     </div>
     <div class="w-1/2 flex justify-center items-center lg:justify-end">
       <a href="https://github.com/zhong1016/sinshui" target="_blank">
